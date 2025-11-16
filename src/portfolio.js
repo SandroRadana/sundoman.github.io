53ca897d3f19b9c910fac230f21ee4fc451dbf56
Sandro Romaddana <sandro.romaddana@gmail.com>
2025-11-16 16:31:42 +0700

    Update 1.6

--- src/portfolio.js
+++ src/portfolio.js
@@ -367,7 +367,7 @@ const achievementSection = {
 const blogSection = {
   title: "Blogs",
   subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+    "Saya Suka Menulis Apa Yang Saya Suka.",
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {

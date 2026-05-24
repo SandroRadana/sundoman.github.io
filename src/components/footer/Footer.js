b0a8a1a5acd561d0d6e3e90abf0be5d4ebb9d571
Sandro Romaddana <sandro.romaddana@gmail.com>
2026-05-24 21:47:46 +0700

    Simplify footer text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,27 +1,14 @@
 import React, {useContext} from "react";
 import "./Footer.scss";
-import {Fade} from "react-reveal";
-import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
   const {isDark} = useContext(StyleContext);
   return (
-    <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Thank you for visiting my portofolio!")}
-        </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {" "}
-          <a
-            href="https://github.com/SandroRadana"
-            target="_blank"
-            rel="noreferrer"
-          >
-          </a>
-        </p>
-      </div>
-    </Fade>
+    <div className="footer-div">
+      <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+        © Sandro Romaddana
+      </p>
+    </div>
   );
 }
